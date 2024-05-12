78684fc01eabb39fe67b1c672f754d1f155a7fae
Daniel Salomon Vazquez Rodriguez <contrabajeando1988@gmail.com>
2024-05-11 18:19:34 -0600

    primer commit

---
+++ src/controllers/articulosController.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export const getArticulos = (req: Request, res: Response) => {};
+export const getArticulosById = (req: Request, res: Response) => {};
+export const addArticulo = (req: Request, res: Response) => {};
